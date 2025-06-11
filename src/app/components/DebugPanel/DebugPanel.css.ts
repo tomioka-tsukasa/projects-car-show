bcf1966047c61a1957798de4224c95ef0f1adde5
tomioka-tsukasa <tsukasa.creator@gmail.com>
2025-06-11 16:48:38 +0900

    feat: DebugPanel 見やすいように調整

--- src/app/components/DebugPanel/DebugPanel.css.ts
+++ src/app/components/DebugPanel/DebugPanel.css.ts
@@ -44,7 +44,7 @@ export const root = style([
     transition: 'opacity 0.3s cubic-bezier(0.0, 0.5, 0.3, 1.0) 0s',
     opacity: 0,
     pointerEvents: 'none',
-    backgroundColor: 'rgba(255, 255, 255, 0.15)',
+    backgroundColor: 'rgba(0, 0, 0,.35)',
     backdropFilter: 'blur(10px)',
   },
   rvw.left(24, '50%'),
