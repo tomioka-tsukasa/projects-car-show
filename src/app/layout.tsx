34452fe805621229102c3aee7b8b3e0241f8b274
tomioka-tsukasa <tsukasa.creator@gmail.com>
2025-06-18 18:46:28 +0900

    add: meta情報

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Cormorant_Garamond, Zen_Old_Mincho } from 'next/font/google'
 import '@/styles/global/globals'
 import StoreProvider from '@/app/store/provider'
 import { GsapManager } from './components/GsapManager/GsapManager'
-import Head from 'next/head'
 
 const zenOldMincho = Zen_Old_Mincho({
   variable: '--font-zen-old-mincho',
@@ -20,6 +19,27 @@ const cormorantGaramond = Cormorant_Garamond({
 export const metadata: Metadata = {
   title: '3D車モデルViewer',
   description: '車の3DモデルをWeb上でリアルタイムプレビューできるサイト。カラー変更やカメラワーク操作が可能で、環境テクスチャ等のディティール調整もできるカスタマイズ機能も付いています。',
+  manifest: '/sakura_camera/assets/home-icon/manifest.json',
+  openGraph: {
+    title: '3D車モデルViewer',
+    siteName: '3D車モデルViewer',
+    description: '車の3DモデルをWeb上でリアルタイムプレビューできるサイト。カラー変更やカメラワーク操作が可能で、環境テクスチャ等のディティール調整もできるカスタマイズ機能も付いています。',
+    url: 'https://projects-car-show.vercel.app/',
+    type: 'website',
+    images: [
+      {
+        url: 'https://projects-car-show.vercel.app/og/og.jpg',
+        width: 1200,
+        height: 630,
+      }
+    ],
+    locale: 'ja_JP',
+  },
+  twitter: {
+    images: 'https://projects-car-show.vercel.app/og/og.jpg',
+    card: 'summary_large_image',
+    description: '車の3DモデルをWeb上でリアルタイムプレビューできるサイト。カラー変更やカメラワーク操作が可能で、環境テクスチャ等のディティール調整もできるカスタマイズ機能も付いています。',
+  }
 }
 
 export default function RootLayout({
@@ -29,14 +49,6 @@ export default function RootLayout({
 }>) {
   return <>
     <StoreProvider>
-      <Head>
-        {/* Open Graph タグ */}
-        <meta property='og:title' content='3D車モデルViewer' />
-        <meta property='og:description' content='車の3DモデルをWeb上でリアルタイムプレビューできるサイト。カラー変更やカメラワーク操作が可能で、環境テクスチャ等のディティール調整もできるカスタマイズ機能も付いています。' />
-        <meta property='og:image' content='https://projects-car-show.vercel.app/og/og.jpg' />
-        <meta property='og:url' content='https://projects-car-show.vercel.app/' />
-        <meta property='og:type' content='website' />
-      </Head>
       <GsapManager />
       <html lang='ja'>
         <body className={`${zenOldMincho.variable} ${cormorantGaramond.variable}`}>
